ce0027230c791e609559e22518ba667c0f26b66f
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2020-11-13 02:10:40 +0100

    Fix formula style issues

--- Formula/drasyl.rb
+++ Formula/drasyl.rb
@@ -1,5 +1,5 @@
 class Drasyl < Formula
-  desc "general-purpose overlay network framework for rapid development of distributed P2P applications"
+  desc "Toolkit for various drasyl-related tasks"
   homepage "https://drasyl.org/"
   url "https://github.com/drasyl-overlay/drasyl/releases/download/v0.3.0/drasyl-0.3.0.zip"
   sha256 "a9bb1c28729dbe5c672ba567cd22e91be570a7e753346c5311dad90ef7f4d953"
@@ -7,7 +7,7 @@ class Drasyl < Formula
 
   livecheck do
     url "https://github.com/drasyl-overlay/drasyl/releases"
-    regex(/drasyl\-(\d+(?:\.\d+)+).zip/)
+    regex(/drasyl-(\d+(?:\.\d+)+).zip/i)
   end
 
   bottle :unneeded
