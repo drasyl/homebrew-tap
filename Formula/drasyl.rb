d9df1b169d694b2bbd8da8bdf0546ad5374ce9e7
Heiko <bornholdt@informatik.uni-hamburg.de>
2021-12-20 02:57:50 +0100

    Add HEAD version
    install via "brew install --HEAD drasyl"

--- Formula/drasyl.rb
+++ Formula/drasyl.rb
@@ -5,6 +5,7 @@ class Drasyl < Formula
   url "https://github.com/drasyl-overlay/drasyl/releases/download/v#{version}/drasyl-#{version}.zip"
   sha256 "e28a909661e3d4008243df4aa4b924bc210fb4a6f4cfd69d33612bec8aca2142"
   license "MIT"
+  head "https://github.com/drasyl-overlay/drasyl.git"
 
   livecheck do
     url "https://github.com/drasyl-overlay/drasyl/releases"
@@ -14,8 +15,14 @@ class Drasyl < Formula
   depends_on "openjdk"
 
   def install
-    rm_f Dir["bin/*.bat"]
-    libexec.install %w[bin lib]
+    if build.head?
+        system "./mvnw --quiet --projects drasyl-cli --also-make -DskipTests -Dmaven.javadoc.skip=true package"
+        system "unzip drasyl-*-SNAPSHOT.zip"
+        libexec.install Dir["drasyl-*-SNAPSHOT/bin"], Dir["drasyl-*-SNAPSHOT/lib"]
+    else
+      rm_f Dir["bin/*.bat"]
+      libexec.install %w[bin lib]
+    end
     (bin/"drasyl").write_env_script "#{libexec}/bin/drasyl", Language::Java.overridable_java_home_env
   end
 
